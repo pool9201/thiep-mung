dfbfdf592c3f84c13c8f56ece1b2d8ae373e13be
pool9201 <93247292+pool9201@users.noreply.github.com>
2022-01-28 11:51:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Thanh Huyền';
+const nameGirl = 'Em yêu♥';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng sinh nhật';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc cậu sinh nhật vui vẻ tràn ngập niềm vui và những nụ cười.Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
+const eventName = 'Chúc mừng năm mới';
+const titleCard = 'Yêu em nhiều lắm';
+const contentCard = 'Năm mới, anh chỉ chúc em được nhiều sức khỏe thôi. Còn về hạnh phúc và niềm vui, anh mong rằng chính mình sẽ là người mang những điều đó đến với em. Yêu em nhiều! Năm mới đến rồi, một năm qua thật đặc biệt với anh và cả em nữa. Năm mới đến đồng nghĩa với việc anh lại được nối dài năm tháng yêu thương chăm sóc em. Anh chúc em tràn đầy hạnh phúc ngọt ngào hãy gìn giữ và nuôi dưỡng tình yêu của chúng mình em nhé.Mãi mãi yêu em ♥';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
